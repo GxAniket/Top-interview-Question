07bdfcaba19595801f9d64df0306ce2503ae5686
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-03-13 22:27:35 +0530

    Add files via upload

---
+++ All Sorting algorithma in  java/CountingSort.java
@@ -0,0 +1,54 @@
+// 6. Counting Sort (Non-comparison)
+
+// Used when numbers are small range
+
+// Time Complexity
+
+// O(n + k)
+
+public class CountingSort {
+
+    static void countingSort(int arr[]){
+
+        int max = arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] > max)
+                max = arr[i];
+        }
+
+        int count[] = new int[max+1];
+
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+
+        int index = 0;
+
+        for(int i=0;i<count.length;i++){
+
+            while(count[i] > 0){
+
+                arr[index] = i;
+                index++;
+                count[i]--;
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {4,2,2,8,3,3,1};
+
+        countingSort(arr);
+
+        System.out.println("Sorted Array:");
+
+        for(int num : arr)
+            System.out.print(num+" ");
+
+    }
+}
---
+++ All Sorting algorithma in  java/Main.java
@@ -0,0 +1,14 @@
+// 7. Java Built-in Sorting (Most Used in Real Projects)
+// Arrays.sort()
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int arr[] = {5,2,8,1,3};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
