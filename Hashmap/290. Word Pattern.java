9e4786d21abbf8577e94e04b2c5cda068b0e729d
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-04-18 23:36:11 +0530

    Update and rename 290. Word Pattern to 290. Word Pattern.java

--- Hashmap/290. Word Pattern.java
+++ Hashmap/290. Word Pattern.java
@@ -1,35 +1,35 @@
-Given a pattern and a string s, find if s follows the same pattern.
+// Given a pattern and a string s, find if s follows the same pattern.
 
-Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
 
-Each letter in pattern maps to exactly one unique word in s.
-Each unique word in s maps to exactly one letter in pattern.
-No two letters map to the same word, and no two words map to the same letter.
+// Each letter in pattern maps to exactly one unique word in s.
+// Each unique word in s maps to exactly one letter in pattern.
+// No two letters map to the same word, and no two words map to the same letter.
  
 
-Example 1:
+// Example 1:
 
-Input: pattern = "abba", s = "dog cat cat dog"
+// Input: pattern = "abba", s = "dog cat cat dog"
 
-Output: true
+// Output: true
 
-Explanation:
+// Explanation:
 
-The bijection can be established as:
+// The bijection can be established as:
 
-'a' maps to "dog".
-'b' maps to "cat".
-Example 2:
+// 'a' maps to "dog".
+// 'b' maps to "cat".
+// Example 2:
 
-Input: pattern = "abba", s = "dog cat cat fish"
+// Input: pattern = "abba", s = "dog cat cat fish"
 
-Output: false
+// Output: false
 
-Example 3:
+// Example 3:
 
-Input: pattern = "aaaa", s = "dog cat cat dog"
+// Input: pattern = "aaaa", s = "dog cat cat dog"
 
-Output: false
+// Output: false
 
 
 //code 
