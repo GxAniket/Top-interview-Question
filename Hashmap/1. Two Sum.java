998f85e7a30355a2c0e5658524b5972d80ea6a04
Aniket Sundriyal <sundriyalaniket@gmail.com>
2026-04-18 23:35:55 +0530

    Update and rename 1. Two Sum to 1. Two Sum.java

--- Hashmap/1. Two Sum.java
+++ Hashmap/1. Two Sum.java
@@ -1,47 +1,47 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+// You can return the answer in any order.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
 
-//dry run
-Dry Run
-nums = [2,7,11,15]
-target = 9
+// //dry run
+// Dry Run
+// nums = [2,7,11,15]
+// target = 9
 
-Step 1
+// Step 1
 
-i = 0
-num = 2
-complement = 7
-map = {2:0}
+// i = 0
+// num = 2
+// complement = 7
+// map = {2:0}
 
-Step 2
+// Step 2
 
-i = 1
-num = 7
-complement = 2
-map contains 2
+// i = 1
+// num = 7
+// complement = 2
+// map contains 2
 
-Return
+// Return
 
-[0,1]
+// [0,1]
 
 //code method one HashMap 
 
